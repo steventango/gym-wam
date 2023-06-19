e160bf8dc71842ea52e86634b5ee813ee7c4768e
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-06-19 11:24:44 -0600

    Remove robot_qvel from observation space

--- gymnasium_robotics/envs/wam/wam_env.py
+++ gymnasium_robotics/envs/wam/wam_env.py
@@ -119,7 +119,7 @@ def get_base_wam_env(RobotEnvClass: Union[MujocoPyRobotEnv, MujocoRobotEnv]):
             obs = np.concatenate(
                 [
                     robot_qpos,
-                    robot_qvel
+                    # robot_qvel
                 ]
             )
 
